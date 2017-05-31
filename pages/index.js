0259be858643ab2b108997097bb5419ab167f75e
enmisac <enmisac@gmail.com>
2017-05-31 00:18:41 -0600

    logré mejorar la url de los posrt

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const PostLink = (props) => {
     return (
         <li>
-            <Link href={`/post?title=${props.title}`}>
+            <Link as={`/post/${props.id}`} href={`/post?title=${props.title}`}>
                 <a>{props.title}</a>
             </Link>
         </li>
@@ -14,10 +14,11 @@ const PostLink = (props) => {
 const Index = () => {
     return (
        <Layout>
+            <h1>My Blog</h1>
             <ul>
-                <PostLink title="Hello next.js" />
-                <PostLink title="Learn Nextjs awesome" />
-                <PostLink title="Deploy apps with zeit" />
+                <PostLink id='hello-nextjs' title="Hello next.js" />
+                <PostLink id='Learn-nextjs-awesome' title="Learn Nextjs awesome" />
+                <PostLink id='deploy-next' title="Deploy apps with zeit" />
             </ul>
        </Layout>
     )
